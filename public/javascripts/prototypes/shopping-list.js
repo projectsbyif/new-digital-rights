a313b252b8b5d1431cd18fcc2248a2613ebb456d
Phil <plangphoto@gmail.com>
2016-09-22 15:24:31 +0100

    getJSON added

--- public/javascripts/prototypes/shopping-list.js
+++ public/javascripts/prototypes/shopping-list.js
@@ -1,3 +1,27 @@
 $(function() {
-  console.log("shopping list prototype js");
+
+  $.getJSON("/public/data/shopping-list.json", function(data) {
+    console.log(data);
+  });
+  
 });
+
+
+
+
+// Finding similar words via regular expressions
+/*
+var name = "apples";
+var search = "apples";
+
+var tests = ['apples', 'apple', 'bag of apples'];
+var isMatch = false;
+
+for (i = 0; i < tests.length; i++) {
+    if (search.test(tests[i])) {
+        isMatch = true;
+    }
+}
+
+alert(isMatch)
+*/
