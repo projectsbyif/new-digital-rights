c8b7968b1c34495f7902e28ecd06ac6e0387422d
Phil <plangphoto@gmail.com>
2016-09-22 17:34:52 +0100

    Shopping list updates
    Building keyup function and searching through JSON

--- public/javascripts/prototypes/shopping-list.js
+++ public/javascripts/prototypes/shopping-list.js
@@ -1,27 +1,27 @@
 $(function() {
 
-  $.getJSON("/public/data/shopping-list.json", function(data) {
-    console.log(data);
+let listTesco = [4]
+let listAsda = [4]
+let listMorrisons = [4]
+let listTimer = []
+
+$("input").keyup(function() {
+  clearInterval(listTimer)
+  listTimer = setTimeout(function(){
+    function checkListItem(){
+      $.getJSON("/public/data/shopping-list.json", function(data) {
+        console.log(data);
+        for(let i = 0; i < data.length; i++) {
+          let market = data[0].json[i];
+            console.log(market);
+            // IF found place in [$(this.index)]
+            // Do this for all supermarkets
+            // Sum up totals
+
+            // Else
+        };
+      });
+    };
+  }, 1500);
   });
-  
 });
-
-
-
-
-// Finding similar words via regular expressions
-/*
-var name = "apples";
-var search = "apples";
-
-var tests = ['apples', 'apple', 'bag of apples'];
-var isMatch = false;
-
-for (i = 0; i < tests.length; i++) {
-    if (search.test(tests[i])) {
-        isMatch = true;
-    }
-}
-
-alert(isMatch)
-*/
