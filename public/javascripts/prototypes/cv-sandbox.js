f264e6e381ca856a16f2ec06a7ba4567cbe82aed
Ian Hutchinson <ian@ihut.cc>
2016-09-22 17:21:06 +0100

    Wired up CV thumb interactions

--- public/javascripts/prototypes/cv-sandbox.js
+++ public/javascripts/prototypes/cv-sandbox.js
@@ -1,3 +1,15 @@
 $(function() {
-  console.log("cv sandbox prototype js");
+  $('.cv_thumbnail a').click(function(e) {
+    e.preventDefault();
+
+    $('.cv_modal').fadeIn(250).css({
+      'display': 'flex'
+    });
+  });
+
+  $('#close_cv').click(function(e) {
+    e.preventDefault();
+
+    $('.cv_modal').fadeOut(250);
+  });
 });
