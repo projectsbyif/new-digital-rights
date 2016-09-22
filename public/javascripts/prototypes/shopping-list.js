532e0032bfe40b94715a9011211da1fd2e49cd7c
Ian Hutchinson <ian@ihut.cc>
2016-09-22 18:42:42 +0100

    Working parse code

--- public/javascripts/prototypes/shopping-list.js
+++ public/javascripts/prototypes/shopping-list.js
@@ -1,27 +1,76 @@
 $(function() {
+  const CURRENCY_SYMBOL = "£";
+  const SEPERATOR = ".";
 
-let listTesco = [4]
-let listAsda = [4]
-let listMorrisons = [4]
-let listTimer = []
+  let listTesco = new Array(10);
+  let listAsda = new Array(10);
+  let listMorrisons = new Array(10);
 
-$("input").keyup(function() {
-  clearInterval(listTimer)
-  listTimer = setTimeout(function(){
-    function checkListItem(){
-      $.getJSON("/public/data/shopping-list.json", function(data) {
-        console.log(data);
-        for(let i = 0; i < data.length; i++) {
-          let market = data[0].json[i];
-            console.log(market);
-            // IF found place in [$(this.index)]
-            // Do this for all supermarkets
-            // Sum up totals
+  let listTimer = new Array(10);
 
-            // Else
-        };
-      });
-    };
-  }, 1500);
+  listTesco.fill(null);
+
+  $('#shopping_list input').keyup(function() {
+    let val = $(this).val();
+    let index = $(this).index();
+
+    clearInterval(listTimer[index]);
+
+    listTimer[index] = setTimeout(function() {
+      checkListItem(val, index);
+    }, 1500);
   });
+
+  function checkListItem(searchQuery, index) {
+    $.getJSON("/public/data/shopping-list.json", function(data) {
+      // Loop through Tesco
+      for (let product in data.tesco) {
+        let query = searchQuery.toLowerCase();
+        let comparator = data.tesco[product].tag.toLowerCase();
+
+        if (query === comparator) {
+          listTesco[index] = data.tesco[product];
+          break
+        } else {
+          listTesco[index] = null;
+        }
+      }
+
+      let totalTesco = 0;
+
+      for (let product in listTesco) {
+        if (listTesco[product]) {
+          totalTesco += listTesco[product].price;
+        }
+      }
+
+      $('#results_tesco .total_cost').text(convertToCurrency(totalTesco));
+
+      // Loop through Asda
+
+
+      // Loop throuh Morrisons
+    });
+  }
+
+  function convertToCurrency(value) {
+    if (!value) {
+        return CURRENCY_SYMBOL + "0" + SEPERATOR + "00";
+    }
+
+    // Ensure value is a string
+    value = value.toString();
+
+    // Pad out values when needed
+    if (value.length == 2) {
+      value = "0" + value;
+    } else if (value.length == 1) {
+      value = "00" + value;
+    }
+
+    // Construct string
+    value = CURRENCY_SYMBOL + value.slice(0, value.length - 2) + SEPERATOR + value.slice(-2);
+
+    return value
+  }
 });
