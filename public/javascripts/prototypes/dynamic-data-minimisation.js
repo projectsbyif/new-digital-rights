f2b2147bc536e09c94e73a82216b1b9940c5d719
Phil <plangphoto@gmail.com>
2016-09-23 15:56:03 +0100

    Updates
    Still tuning to show peaks and troughs.

--- public/javascripts/prototypes/dynamic-data-minimisation.js
+++ public/javascripts/prototypes/dynamic-data-minimisation.js
@@ -1,19 +1,32 @@
 $(function() {
+    var d = new Date();
+    var n = d.getHours();
+    var day = d.getDay();
 
     function randomIntInc(low, high) {
         return Math.floor(Math.random() * (high - low + 1) + low);
     }
 
-    function randomNumber(min, max, time, name) {
-        var name = new Array(time);
-        for (var i = 0; i < name.length; i++) {
-            name[i] = randomIntInc(min, max)
-            console.log(name)(time)
+    function create(min, max, period, time) {
+        let kwh = new Array(period);
+        for (var i = 0; i < kwh.length; i++) {
+            kwh[i] = generateTime(min, max);
+        }
+        console.log(kwh);
+    }
+
+    function generateTime(hour) {
+        if (hour <= 10 && hour >= 6) {
+            return Math.floor((Math.random() * 50) + 80);
+        } else {
+            return randomIntInc(hour, day);
         }
     }
-    randomNumber(0.1, 1.0, 24, "day");
-    randomNumber(3, 7, 7, "week");
-    //needs to factor in 29/30/31 day months
-    randomNumber(100, 200, 12, "month");
-    randomNumber(1800, 2500, 1, "year");
+
+    generateTime(n);
+    create(40, 100, 24, "day");
+    create(400, 600, 7, "week");
+    create(4500, 5000, 4, "month");
+    create(20000, 24000, 12, "year");
+    create(20000, 24000, 5, "5 years");
 });
