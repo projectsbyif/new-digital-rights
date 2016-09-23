b4b08c87425d8a2bc2304411a3081a60a946034e
Phil <plangphoto@gmail.com>
2016-09-23 11:24:29 +0100

    Beginning to build random data
    Beginning to build random data for the energy minimisation prototype

--- public/javascripts/prototypes/dynamic-data-minimisation.js
+++ public/javascripts/prototypes/dynamic-data-minimisation.js
@@ -1,3 +1,19 @@
 $(function() {
-  console.log("dynamic data minimisation prototype js");
+
+    function randomIntInc(low, high) {
+        return Math.floor(Math.random() * (high - low + 1) + low);
+    }
+
+    function randomNumber(min, max, time, name) {
+        var name = new Array(time);
+        for (var i = 0; i < name.length; i++) {
+            name[i] = randomIntInc(min, max)
+            console.log(name)(time)
+        }
+    }
+    randomNumber(0.1, 1.0, 24, "day");
+    randomNumber(3, 7, 7, "week");
+    //needs to factor in 29/30/31 day months
+    randomNumber(100, 200, 12, "month");
+    randomNumber(1800, 2500, 1, "year");
 });
