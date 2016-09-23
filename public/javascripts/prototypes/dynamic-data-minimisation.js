9ef502bf50f855432663bfe2bbc1256a06af677d
Ian Hutchinson <ian@ihut.cc>
2016-09-23 13:43:20 +0100

    Flying through space and time

--- public/javascripts/prototypes/dynamic-data-minimisation.js
+++ public/javascripts/prototypes/dynamic-data-minimisation.js
@@ -1,3 +1,11 @@
 $(function() {
-  console.log("dynamic data minimisation prototype js");
+  let timerIncrementTime;
+  let now = moment().hour(0).minute(0).second(0);
+
+  timerIncrementTime = setInterval(function() {
+    now.add(1, 'h');
+
+    $('#present_date').text(now.format("dddd, D MMMM YYYY"));
+    $('#present_time').text(now.format("HH:mm"));
+  }, 25);
 });
