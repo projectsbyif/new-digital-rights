f22bc3994342b947623df8af2c961366fd20ea0b
Ian Hutchinson <ian@ihut.cc>
2016-09-26 18:42:42 +0100

    Homepage and about page

--- config/config.js
+++ config/config.js
@@ -3,4 +3,7 @@ var config = {};
 config.site_name = "GDPR microsite";
 config.site_description = "Prototypes that show the possibilities created by GDPR";
 
+config.homepage_headline = "Designing for new digital rights";
+config.homepage_subheading = "As our rights as digital citizens change, we explore the new kind of services that might emerge.";
+
 module.exports = config;
