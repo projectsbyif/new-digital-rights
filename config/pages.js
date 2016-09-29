26d9083f7a379996cf38213190c2237ac3562a54
Ian Hutchinson <ian@ihut.cc>
2016-09-29 12:52:54 +0100

    New copy

--- config/pages.js
+++ config/pages.js
@@ -3,45 +3,45 @@ var content = {};
 content = [
   {
     "title": "Dynamic data minimisation",
-    "description": "Gradual loss of detail in a dataset over time, balancing usefulness with privacy. Especially useful in IoT.",
+    "description": "<p>Devices can store more information about a greater number of things over time. But while some data gets more useful over time, other data potentially our privacy and security.</p><p>Dynamic data minimisation is a design pattern that takes detailed, real-time data and averages out historic data over time. The result balances long term use with the privacy implications of storing data over time. This is especially useful for things like smart meters in rented houses, where tenants would be able to see the general trends of a property with knowing detailed information about previous tenants habits.</p><p>See the ICO website to read more about the <a href=\"https://ico.org.uk/for-organisations/data-protection-reform/overview-of-the-gdpr/individuals-rights/the-right-to-erasure/\">right to erasure</a> in the General Data Protection Regulation.</p>",
     "permalink": "dynamic-data-minimisation",
-    "rights": "Right to erasure"
+    "tagline": "A design pattern for data balancing privacy and value"
   },
   {
-    "title": "Shopping list",
-    "description": "A unspecific shopping list that is processed into shopping baskets for specific retails.",
-    "permalink": "shopping-list",
-    "rights": "Right to data portability"
+    "title": "Portable shopping list",
+    "description": "<p>By encouraging companies to make users data more portable, new behaviours will emerge that change how we shop and interact with services online.</p><p>Price comparison sites today rely on integrating with APIs and scraping adat from dozens of different sites. Establishing standards for things like shopping lists or viewing history is a response to regulation that encourages data portability which will open up market innovation. It could see people changing shopping suppliers based on factors including price or delivery time as well as things like the quality of recommendation algorithm.</p><p>Visit the ICO website to read more about <a href=\"https://ico.org.uk/for-organisations/data-protection-reform/overview-of-the-gdpr/individuals-rights/the-right-to-data-portability/\">the right to data portability</a> in the General Data Protection Regulation.</p>",
+    "permalink": "portable-shopping-list",
+    "tagline": "Taking your data with you from service to service"
   },
   {
-    "title": "CV sandbox",
-    "description": "A way of checking the acceptance of a CV by a pre-screener that's seperate from the actual pre-screener.",
-    "permalink": "cv-sandbox",
-    "rights": "Rights around automated decision making"
+    "title": "Cover letter sandbox",
+    "description": "<p>If algorithms are made more accountable to the public it creates an opportunity to build products and tools that show off how those algorithms work.</p><p>Automated CV pre-screening is already gaining traction in large organisations. The CV sandbox allows candidates to submit their CV to the system for a test run, so candidates can fine tune their application. A report shows how and why they’ve been accepted or rejected by the algorithm, exposing the rules it is checking the CV and explaining them clearly and simply.</p><p>See the ICO website to read more about <a href=\"https://ico.org.uk/for-organisations/data-protection-reform/overview-of-the-gdpr/individuals-rights/rights-related-to-automated-decision-making-and-profiling/\">rights related to automated decision making and profiling</a> in the General Data Protection Regulation.</p>",
+    "permalink": "cover-letter-sandbox",
+    "tagline": "Exposing automated decision making to users"
   },
   {
-    "title": "Insight permissions",
-    "description": "A design pattern that shows what insight about a person has been used to show a particular advert. It provides a way of opting out from the use of that metric.",
-    "permalink": "insight-permissions",
-    "rights": "Right to restrict processing"
+    "title": "Transparent targeting",
+    "description": "<p>Showing users why they’ve been targeted by an ad or service is going to become an important way for brands to increase trust. That’s especially true as our right to restrict processing is enforced.</p><p>The prototype shows how this might appear in adverts. People can see what data about them led to the ad being targeted in the first place, and gives them an opportunity to correct that information, or stop the network using it if they think it’s inaccurate or misleading.</p><p>See the ICO website to read more about <a href=\"https://ico.org.uk/for-organisations/data-protection-reform/overview-of-the-gdpr/individuals-rights/the-right-to-restrict-processing/\">the right to restrict processing</a> in the General Data Protection Regulation.</p>",
+    "permalink": "transparent-targeting",
+    "tagline": "Explaining why we see the ads we see, and changing that"
   },
   {
-    "title": "Online presence declaration",
-    "description": "A form that lets someone choose how their online accounts are handled after their death. It would be carried out by a public institution.",
-    "permalink": "online-presence-declaration",
-    "rights": "Right to rectification"
+    "title": "Public guardian for digital identity",
+    "description": "<p>When companies sell our information on, there’s no chance to update that information. The GDPR places the burden of correcting it back on the company that sold it. But what about when we’re no longer here to correct it?</p><p>Rather than making families responsible for contact each service, our ‘digital identity will’ imagines a public guardian whose responsibility is to ensure that companies process the data of dead users according to the wishes of the deceased. Users can determine if they want their data to be deleted, archived, made accessible by a limited number of people or something in between, and follows traditional precedents like the Office for the Public Guardian.</p><p>See the ICO website to read more about <a href=\"https://ico.org.uk/for-organisations/data-protection-reform/overview-of-the-gdpr/individuals-rights/the-right-to-rectification/\">the right to rectification</a> in the General Data Protection Regulation.</p>",
+    "permalink": "public-guardian",
+    "tagline": "How services handle the death of a user"
   },
   {
     "title": "Activity log",
-    "description": "When data is shared, a receipt is made that shows who the data has been shared with, for how long and why. Events are recorded like when the data is viewed, when it has acheived its reason to be shared and when it has been destroyed.",
+    "description": "<p>As people are given more insight about what information is stored about them and who else accesses it we need to think about ways of showing that that don’t overwhelm people.</p><p>The activity log is one way of doing this. It shows people information about how their data has been used by an organisation in a clear, accessible way. It also displays how long the information will be kept by the people who have accessed it and what will happen to it after that point.</p><p>See the ICO website to read more about <a href=\"https://ico.org.uk/for-organisations/data-protection-reform/overview-of-the-gdpr/individuals-rights/the-right-to-be-informed/\">the right to be informed</a> and <a href=\"https://ico.org.uk/for-organisations/data-protection-reform/overview-of-the-gdpr/individuals-rights/the-right-of-access/\">the right of access</a> in the General Data Protection Regulation.</p>",
     "permalink": "activity-log",
-    "rights": "Right to access / right to be informed"
+    "tagline": "Seeing who accesses our data, and what for"
   },
   {
-    "title": "Digital blinds",
-    "description": "A section in a WiFi routers settings, where people can choose what digital services are allowed to gather data about their home and its surroundings.",
-    "permalink": "digital-blinds",
-    "rights": "Right to object"
+    "title": "Property privacy protocol",
+    "description": "<p>Websites use robot inclusion and exclusion standards to determine how much information can be read by other machines. Properties should be able to do the same, proactively managing their privacy online rather than doing it after the fact.</p><p>If someone doesn’t want photos of their home processed by streetview cars, or the mac address of their router stored by devices, they should be say so. Similarly, if you manage a property that would benefit from better data – a farm, for instance – you could use a service that helps automated devices to capture better quality information.</p><p>See the ICO website to read more about <a href=\"https://ico.org.uk/for-organisations/data-protection-reform/overview-of-the-gdpr/individuals-rights/the-right-to-object/\">the right to object</a> in the General Data Protection Regulation.</p>",
+    "permalink": "property-privacy-control",
+    "tagline": "Respecting privacy when collecting data"
   }
 ];
 
