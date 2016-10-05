ffad48504a8a28b4983ac572b531f11a4c266ffb
Phil <plangphoto@gmail.com>
2016-10-05 18:17:53 +0100

    Updates for the property privacy prototype

--- public/javascripts/prototypes/property-privacy-control.js
+++ public/javascripts/prototypes/property-privacy-control.js
@@ -1,2 +1,9 @@
 $(function() {
+  $('.next_button').click(function(e) {
+    e.preventDefault();
+
+    $('#blinds_one').fadeOut(250, function() {
+      $('#blinds_two').fadeIn(250);
+    });
+  });
 });
