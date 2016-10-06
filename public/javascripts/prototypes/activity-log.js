4bf8d76d1746924405211ed50b36621320a4f235
Ian Hutchinson <ian@ihut.cc>
2016-10-06 15:48:12 +0100

    Remove old activity log

--- public/javascripts/prototypes/activity-log.js
+++ public/javascripts/prototypes/activity-log.js
@@ -1,17 +1,3 @@
 $(function() {
-  $('#show_receipt').click(function(e) {
-    e.preventDefault();
 
-    $('.activity_log_timeline').fadeOut(250, function() {
-      $('.activity_log_receipt').fadeIn(250);
-    });
-  });
-
-  $('#show_activity_log').click(function(e) {
-    e.preventDefault();
-
-    $('.activity_log_receipt').fadeOut(250, function() {
-      $('.activity_log_timeline').fadeIn(250);
-    });
-  });
 });
