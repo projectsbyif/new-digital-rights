a683cf81c71388e43fc8ab254725b4cd75621fc8
Ian Hutchinson <ian@ihut.cc>
2016-10-13 15:31:28 +0100

    Make prototype look like a social network feed

--- public/javascripts/prototypes/transparent-targeting.js
+++ public/javascripts/prototypes/transparent-targeting.js
@@ -2,35 +2,25 @@ $(function() {
   $('#show_permissions_dialog').click(function(e) {
     e.preventDefault();
 
-    $('.permissions_dialog').fadeIn(250);
-  });
-
-  $('.permissions_dialog .hide').click(function(e) {
-    e.preventDefault();
-
-    $('.permissions_dialog').fadeOut(250);
+    $('.sample_container').fadeOut(250, function() {
+      $('.permissions_container').fadeIn(250);
+    });
   });
 
   $('#allow_ad').click(function(e) {
     e.preventDefault();
 
-    $('.permissions_dialog').fadeOut(250)
-
-    $('#allow_ad_confirmation').css({
-      'opacity': 1
-    }).delay(2000).animate({
-      'opacity': 0
-    }, 250);
+    $('.permissions_container').fadeOut(250, function(e) {
+      $('#show_permissions_dialog').text("Thank you for your feedback.");
+      $('.sample_container').fadeIn(250);
+    });
   });
 
   $('#deny_ad').click(function(e) {
     e.preventDefault();
 
-    $('.permissions_dialog').fadeOut(250);
-    $('#show_permissions_dialog').fadeOut(250);
-
-    $('#deny_ad_confirmation').css({
-      'opacity': 1
+    $('.permissions_container').fadeOut(250, function(e) {
+      $('.deny_ad_container').fadeIn(250);
     });
   });
 });
