f148da822050b3a6e78a15cff24bc7f8f8c8aad6
Ian Hutchinson <ian@ihut.cc>
2016-10-13 12:24:42 +0100

    Better animation timings

--- public/javascripts/prototypes/data-minimisation.js
+++ public/javascripts/prototypes/data-minimisation.js
@@ -2,19 +2,16 @@ $(function() {
   $('#list_click_yes').click(function(e) {
     e.preventDefault();
 
-    $('.location').fadeOut(250);
-
     $('#map').css({
       'background-image': "url('/public/images/brighton-filtered.png')"
     });
 
-    $('#list_click_yes, #list_click_no').fadeOut(250);
+    $('#list_click_yes, #list_click_no').fadeOut(250, function() {
+      $('#list_click_save').delay(750).fadeIn(500);
+    });
 
-    $('.decision_text').replaceWith("<p>Detailed location data from your trip has been deleted.")
-
-    $('.preference_text').replaceWith("<p>Save this preference for future trips?")
-
-    $('#list_click_save').fadeIn(1200);
+    $('.decision_text').replaceWith("<p>Detailed location data from your trip has been deleted.");
+    $('.preference_text').replaceWith("<p>Save this preference for future trips?");
   });
 
   $('#list_click_no').click(function(e) {
@@ -28,7 +25,8 @@ $(function() {
   $('#list_click_save').click(function(e) {
     e.preventDefault();
 
-    $('#list_click_save').fadeOut(250);
-    $('#list_click_saved').fadeIn(1200);
+    $('#list_click_save').fadeOut(250, function() {
+      $('#list_click_saved').fadeIn(500);
+    });
   });
 });
