ff666a3c2710ec4e3878a244034ed43f415cdad8
Phil <plangphoto@gmail.com>
2016-10-19 17:32:54 +0100

    Updated quote amounts

--- public/javascripts/prototypes/single-trip-insurance-checker.js
+++ public/javascripts/prototypes/single-trip-insurance-checker.js
@@ -44,16 +44,9 @@ $(function() {
 
   function recalculate() {
     let quoteAmount = 1000;
-    let roadSpeed = $('.section #road_speed .selected').text();
     let roadType = $('.section #road_type .selected').text();
     let passengers = $('.section #passengers .selected').text();
-    let time = $('.section #time .selected').text();
 
-    if (roadSpeed === "Slow") {
-      quoteAmount += 500;
-    } else if (roadSpeed === "Fast") {
-      quoteAmount += 750;
-    }
 
     if (roadType === "Minor roads") {
       quoteAmount += 550;
@@ -63,19 +56,8 @@ $(function() {
 
     if (passengers === "Driver only") {
       quoteAmount += 100;
-    } else if (passengers === "Driver + Passengers") {
+    } else if (passengers === "+ Passengers") {
       quoteAmount += 500;
-    }
-
-    // Choose best route
-    if (roadSpeed === "Fast" && roadType === "Major roads") {
-      useRoute = 0;
-    } else if (roadSpeed === "Fast" && roadType === "Minor roads") {
-      useRoute = 1;
-    } else if (roadSpeed === "Slow" && roadType === "Major roads") {
-      useRoute = 2;
-    } else if (roadSpeed === "Slow" && roadType === "Minor roads") {
-      useRoute = 3;
     }
 
     populateMeta(
@@ -83,17 +65,11 @@ $(function() {
       predefinedRoutes[useRoute].distance,
       predefinedRoutes[useRoute].time);
 
-
-    if (time === "Day") {
-      quoteAmount += 100;
-      $('#route_depart').text("Leave between 08:00 and 18:59.");
-    } else if (time === "Night") {
-      quoteAmount += 450;
-      $('#route_depart').text("Leave between 19:00 and 07:59.");
-    }
-
     $('#quote_amount').text(convertToCurrency(quoteAmount));
+    $('#quote_amount_2').text(convertToCurrency(quoteAmount));
+    $('#quote_amount_3').text(convertToCurrency(quoteAmount));
   }
+
 
   function populateMeta(identifier, distance, time) {
     $('#route_identifier').text(identifier);
@@ -131,6 +107,7 @@ $(function() {
     $('.hide_journey').fadeOut(250, function() {
       $('.hide_route').delay(750).fadeIn(500);
     });
+
   });
 
   $('#quote_yes').click(function(e) {
@@ -139,6 +116,8 @@ $(function() {
     $('.hide_route').fadeOut(250, function() {
       $('.hide_options').delay(750).fadeIn(500);
     });
+
+    recalculate();
   });
 
   $('#quote_no').click(function(e) {
@@ -155,5 +134,7 @@ $(function() {
     $('.hide_options').fadeOut(250, function() {
       $('.hide_quote').delay(750).fadeIn(500);
     });
+
+    recalculate();
   });
 });
