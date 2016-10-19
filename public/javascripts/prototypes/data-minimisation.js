87d2d6c104e754a6b37e2bec2f4524906eef3a1a
Phil <plangphoto@gmail.com>
2016-10-19 16:55:00 +0100

    Removed 'save' options
    Streamlined

--- public/javascripts/prototypes/data-minimisation.js
+++ public/javascripts/prototypes/data-minimisation.js
@@ -6,12 +6,10 @@ $(function() {
       'background-image': "url('/public/images/brighton-filtered.jpg')"
     });
 
-    $('#list_click_yes, #list_click_no').fadeOut(250, function() {
-      $('#list_click_save').delay(750).fadeIn(500);
-    });
-
     $('.decision_text').replaceWith("<p>Detailed location data from your trip has been deleted.");
-    $('.preference_text').replaceWith("<p>Save this preference for future trips?");
+
+    $('#list_click_yes, #list_click_no').fadeOut(250);
+
   });
 
   $('#list_click_no').click(function(e) {
@@ -22,11 +20,4 @@ $(function() {
     $('#list_click_yes, #list_click_no').fadeOut(250);
   });
 
-  $('#list_click_save').click(function(e) {
-    e.preventDefault();
-
-    $('#list_click_save').fadeOut(250, function() {
-      $('#list_click_saved').fadeIn(500);
-    });
-  });
 });
