9f6447161b0b5ecc4220a64f7c1bd447fd041ee3
Phil <plangphoto@gmail.com>
2016-10-28 10:34:53 +0100

    Copy push
    Updates from Sarah and Matt

--- config/pages.js
+++ config/pages.js
@@ -3,50 +3,43 @@ var content = {};
 content = [
   {
     "title": "Data minimisation",
-    "top_text": "<p>Location data is really important for lots of services, particularly when you’re trying to get somewhere or find a place you need to be. But extremely detailed information about where we've been gets stored indefinitely. That's a big security risk.</p><p>People should have the option of making that information less granular over time.</p>",
-    "bottom_text": "<p>Data minimisation tools keep us more secure. This kind of data minimisation could affect other settings too. For instance, our devices could ‘forget’ old Wi-Fi networks we've joined, which leave you open to data theft.</p><p>Meanwhile, services which use less specific information – like photo libraries – will still be useful. After all, 'Your trip to Brighton' makes more sense than 'Your trips to 7 Kensington Gardens, 52 Ship Street, and 11 Queens Road'.</p>",
+    "top_text": "<p>Location data is crucial for lots of services, particularly when you’re trying to get somewhere or find a place you need to be. But that detailed information about where you’ve been is often stored forever.</p><p>GDPR gives people the ‘right to erasure’, the ability to remove personal data if it’s no longer necessary. But sometimes it’s not an all-or-nothing decision. People should have the option of making that information less granular over time. After all, 'Your trip to Brighton' makes more sense than 'Your trips to 7 Kensington Gardens, 52 Ship Street, and 11 Queens Road'.</p>",
     "permalink": "data-minimisation",
     "tagline": "Balancing privacy and value"
   },
   {
     "title": "Portable shopping list",
-    "top_text": "<p>The longer we shop with a particular store, the more our data improves their recommendations. But the cost of that loyalty is that we don’t change our grocer very often, even when it's less convenient to use a particular chain.</p><p>Data portability means people can think about their habits online in a different way.</p>",
-    "bottom_text": "<p>People have more power in the market if they can take their shopping lists or purchase histories with them. They can also make decisions based on factors beyond price, like fuel emissions or delivery time.</p>",
+    "top_text": "<p>The longer we shop with a particular store, the more our historic data contributes to the recommendations we get. But the cost of that loyalty is that we don’t change who we shop with very often, even when it's more convenient or cheaper to use someone else. </p><p>People have more power in the market if they can take their shopping lists or purchase histories with them. The GDPR gives people the right to data portability, so we’ve imagined what this might look like for online food shopping.</p>",
     "permalink": "portable-shopping-list",
     "tagline": "Taking your data with you"
   },
   {
     "title": "Single trip insurance checker",
-    "top_text": "<p>As the insurance industry adapts to the gig economy and innovations like self-driving cars, getting quotes for short journeys is going to get increasingly common. It’s important people understand the premiums they’re paying, and how their choices affect the decisions insurance algorithms make.</p><p>The insurance checker exposes what goes into the decision about the cost and risk involved in a route.</p>",
-    "bottom_text": "<p>The critical thing about tools like this is that is that people can object to a decision.</p><p>While what a program decides about the risk involved in using a particular road might not be very controversial, the decisions it makes based on someone’s age, gender or background might result in people raising strong objections.</p><p>Clearly explaining why an algorithm is giving an answer will increase people's confidence in that answer. It will also expose the biases written into it.</p>",
+    "top_text": "<p>As the insurance industry adapts to the gig economy and an increase in car sharing, getting insurance quotes for one-off journeys will get more and more common. The GDPR gives people the right to understand automated decision making. We’ve imagined what this looks like when you get insurance for a single trip.</p><p>The insurance checker explains why a quote costs what it does, and gives you the option to object if a decision has been made unfairly.</p>",
     "permalink": "single-trip-insurance-checker",
     "tagline": "Exposing automated decision making"
   },
   {
     "title": "Transparent targeting",
-    "top_text": "<p>When we see adverts online it’s usually clear they have some relationship to our browsing history. But that connection is rarely explicit.</p><p>By revealing why we’ve been targeted by a particular ad we can learn more about what companies know about us. When that doesn't feel right we should be able to opt out of having that data processed.</p>",
-    "bottom_text": "<p>The cookie law was an attempt to protect consumers by educating them about tracking. But the implementation of cookie banners just made the issue more obscure.</p><p>Transparent targeting exposes the information advertisers have about a person straight away. It puts them in control of deciding whether that data should be used or not.</p>",
+    "top_text": "<p>Advertisers use lots of pieces of information to target their ads. But sometimes you’ll see something that isn’t relevant anymore, like an advert for a new car when you’ve just bought one.</p><p>The GDPR gives people the right to restrict processing. So we’ve imagined an advert that allows you to tell the advertiser some information they have is no longer relevant.</p>",
     "permalink": "transparent-targeting",
     "tagline": "Giving people power over the data advertisers use"
   },
   {
     "title": "Guardian for digital identity",
-    "top_text": "<p>The law is very clear about what happens to our belongings when we die, but much vaguer when it comes to digital identity.</p><p>We imagined the role a digital public might have. What formal process might people go through to transfer their accounts to people they trust, and what rules should services abide by?</p>",
-    "bottom_text": "<p>Today, if you give a service your address and they sell those details on, it can be extremely difficult to correct that when you move. That makes it very hard to imagine having much say in what happens to your data after you die.</p><p>The General Data Protection Regulation makes it clear that people do have the right to rectify data. Organisations need to think about how they do that in ways that allow for much more extreme changes of circumstance than just moving home.</p>",
+    "top_text": "<p>The law is clear about what happens to our belongings when we die, but much vaguer when it comes to our digital property. Some companies have started to think about this, but there’s very little public oversight.</p><p>We imagined how a digital public guardian could help. When you die, could their services delete your private messages from Facebook before transferring your account to someone you trust?</p><p>The GDPR states that people will have the right to rectify data, but that could imply quite narrow changes (like changing your address when you move). Organisations need to prepare for more extreme changes of circumstance.</p><i><a>Inspired by Tom Steinberg’s <a>‘Digital Graveyards’</a></i>.",
     "permalink": "public-guardian",
     "tagline": "What happens to our data after we die"
   },
   {
     "title": "Data access tracker",
-    "top_text": "<p>When we do things like rent a flat or buy a house, we hand over a lot of personal data to strangers. It's not clear why we're doing that, or who will be able to see it.</p><p>The data access tracker shows you what information you’ve sent, why someone else needs it, and who has used it. If people have access and it isn't clear why then you can report them.</p>",
-    "bottom_text": "<p>When we give companies personal data it should be extremely clear why we're doing that. As more sophisticated ways of doing things like verifying identity become more common, the amount of data we give services should be getting smaller.</p><p>Meanwhile, with large-scale security breaches becoming more frequent, people should be able to see who has accessed data about them and raise an alarm when it isn't clear why.</p>",
+    "top_text": "<p>When you rent a flat or buy a house, you have to hand over a lot of personal information. You might be asked to share bank statements or payslips to show you can afford the rent. But you don’t know who uses that information, or how long they keep it for.</p><p>GDPR grants the right of access and the right to be informed. These broadly mean people will have the right to know who is processing their data and why. We’ve imagined what this looks like in the context of renting a flat.</p><p>The data access tracker shows you what information you’ve sent, why someone else needs it, and who has accessed it. If you see unusual behaviour you can report the problem.</p>",
     "permalink": "data-access-tracker",
     "tagline": "Seeing who accesses our data, and what for"
   },
   {
     "title": "Home privacy settings",
-    "top_text": "<p>A wireless router has the ability to broadcast information to other devices. That information could instruct things like Google’s mapping cars not to store photographs of a particular property, or record wireless network details.</p><p>These privacy settings could be part of how you set up Wi-Fi in your home, making digital privacy an everyday part of life.</p>",
-    "bottom_text": "<p>Objecting to the way a company uses information is often difficult, and might involve hiring solicitors to act on your behalf.</p><p>Making it a simple digital service makes it easier for people to proactively protect themselves, and register their objection to unchecked data harvesting.</p>",
+    "top_text": "<p>Routers can broadcast information. You could set your home router to instruct Uber’s mapping cars whether to store photography of your house or details of your home Wi-Fi.</p><p>These privacy settings could be part of how you set up Wi-Fi in your home, giving you the choice about whether you want a company to store and use that information or not. This is an example of how the GDPR’s right to restrict processing might work.</p>",
     "permalink": "home-privacy-settings",
     "tagline": "Respecting privacy when collecting data"
   }
