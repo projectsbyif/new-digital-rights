25d4ecad1b2a8fca61a9469e59e5e9cafba9f4a1
Phil <plangphoto@gmail.com>
2016-10-29 10:28:27 +0100

    Push updates

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.site_name = "Designing for new digital rights";
-config.site_description = "The General Data Protection Regulation gives people new rights around data, privacy and security.";
+config.site_description = "The General Data Protection Regulation gives people new rights for personal data.";
 
 config.homepage_headline = "Designing for new digital rights";
 config.homepage_subheading = config.site_description;
