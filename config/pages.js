2cf9cbed4ad7297eb83f9bc99b192b369facf8de
Ian Hutchinson <ian@ihut.cc>
2018-02-27 09:42:44 +0000

    Update copy

--- config/pages.js
+++ config/pages.js
@@ -11,7 +11,7 @@ content = [
     "title": "Portable shopping list",
     "top_text": "<p>The longer we shop with a particular store, the more our historic data contributes to the recommendations we get. But the cost of that loyalty is that we don’t change who we shop with very often, even when it's more convenient or cheaper to use someone else. </p><p>People have more power in the market if they can take their shopping lists or purchase histories with them. The GDPR gives people the right to data portability, so we’ve imagined what this might look like for online food shopping.</p>",
     "permalink": "portable-shopping-list",
-    "tagline": "Taking your data with you"
+    "tagline": "Taking data about you to other services"
   },
   {
     "title": "Single trip insurance checker",
@@ -23,19 +23,19 @@ content = [
     "title": "Work with us",
     "top_text": "<p>The cookie law was an attempt to protect consumers by making them aware about how their information is collected and give them the choice to opt out. So cookie banners became the way most websites comply with that law, but they just don’t work. No-one really understands why they’re there or what agreeing to them means.</p><p>If we don’t start exploring what the General Data Protection Regulation means for people, the same thing that happened with the cookie law will happen again.</p><p>These new rights have the potential to improve how our digital products and services work. If you want to explore them with us, <a style='border-bottom: 1px solid #ff5e23; color: #000; text-decoration: none;' href='mailto:hello@projectsbyif.com'>get in touch</a>.</p>",
     "permalink": "work-with-us",
-    "tagline": "Giving people power over the data advertisers use"
+    "tagline": "Work with IF to use GDPR to innovate in your marketplace"
   },
   {
     "title": "Guardian for digital identity",
     "top_text": "<p>The law is clear about what happens to our belongings when we die, but much vaguer when it comes to our digital property. Some companies have started to think about this, but there’s very little public oversight.</p><p>We imagined how a digital public guardian could help. When you die, could their services delete your private messages from Facebook before transferring your account to someone you trust?</p><p>The GDPR states that people will have the right to rectify data, but that could imply quite narrow changes (like changing your address when you move). Organisations need to prepare for more extreme changes of circumstance.</p><a style='border-bottom: 1px solid #ff5e23; color: #000; text-decoration: none;' href='https://medium.com/@TomSteinberg/digital-public-institutions-part-2-new-organisations-we-might-see-soon-89c4560cd356#.bjczykdt4'>Inspired by Tom Steinberg’s ‘Digital Graveyards’</a>.",
     "permalink": "public-guardian",
-    "tagline": "What happens to our data after we die"
+    "tagline": "What happens to data about us after we die"
   },
   {
     "title": "Data access tracker",
     "top_text": "<p>When you rent a flat or buy a house, you have to hand over a lot of personal information. You might be asked to share bank statements or payslips to show you can afford the rent. But you don’t know who uses that information, or how long they keep it for.</p><p>GDPR grants the right of access and the right to be informed. These broadly mean people will have the right to know who is processing their data and why. We’ve imagined what this looks like in the context of renting a flat.</p><p>The data access tracker shows you what information you’ve sent, why someone else needs it, and who has accessed it. If you see unusual behaviour you can report the problem.</p>",
     "permalink": "data-access-tracker",
-    "tagline": "Seeing who accesses our data, and what for"
+    "tagline": "Seeing who accesses data about you, and what for"
   },
   {
     "title": "Home privacy settings",
